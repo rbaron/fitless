09b8f3b502276e89caa5aa3847d4d70a5985d374
rbaron <rbaron@rbaron.net>
2018-05-13 17:19:00 +0200

    Added twi_scanner example.

---
+++ twi_scanner/main.c
@@ -0,0 +1,142 @@
+/**
+ * Copyright (c) 2016 - 2017, Nordic Semiconductor ASA
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form, except as embedded into a Nordic
+ *    Semiconductor ASA integrated circuit in a product or a software update for
+ *    such product, must reproduce the above copyright notice, this list of
+ *    conditions and the following disclaimer in the documentation and/or other
+ *    materials provided with the distribution.
+ *
+ * 3. Neither the name of Nordic Semiconductor ASA nor the names of its
+ *    contributors may be used to endorse or promote products derived from this
+ *    software without specific prior written permission.
+ *
+ * 4. This software, with or without modification, must only be used with a
+ *    Nordic Semiconductor ASA integrated circuit.
+ *
+ * 5. Any software provided in binary form under this license must not be reverse
+ *    engineered, decompiled, modified and/or disassembled.
+ *
+ * THIS SOFTWARE IS PROVIDED BY NORDIC SEMICONDUCTOR ASA "AS IS" AND ANY EXPRESS
+ * OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY, NONINFRINGEMENT, AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL NORDIC SEMICONDUCTOR ASA OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+ * GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+/** @file
+ * @defgroup tw_scanner main.c
+ * @{
+ * @ingroup nrf_twi_example
+ * @brief TWI Sensor Example main file.
+ *
+ * This file contains the source code for a sample application using TWI.
+ *
+ */
+
+#include <stdio.h>
+#include "boards.h"
+#include "app_util_platform.h"
+#include "app_error.h"
+#include "nrf_drv_twi.h"
+#include "nrf_delay.h"
+#include "nrf_gpio.h"
+#include "SEGGER_RTT.h"
+
+/* TWI instance ID. */
+#define TWI_INSTANCE_ID     0
+
+ /* Number of possible TWI addresses. */
+ #define TWI_ADDRESSES      127
+
+
+#define RST_PIN 24
+#define SCL_PIN 22
+#define SDA_PIN 21
+
+
+/* TWI instance. */
+static const nrf_drv_twi_t m_twi = NRF_DRV_TWI_INSTANCE(TWI_INSTANCE_ID);
+
+
+/**
+ * @brief TWI initialization.
+ */
+void twi_init (void)
+{
+    ret_code_t err_code;
+
+    const nrf_drv_twi_config_t twi_config = {
+       .scl                = SCL_PIN,
+       .sda                = SDA_PIN,
+       .frequency          = NRF_TWI_FREQ_100K,
+       .interrupt_priority = APP_IRQ_PRIORITY_HIGH,
+       .clear_bus_init     = false
+    };
+
+    err_code = nrf_drv_twi_init(&m_twi, &twi_config, NULL, NULL);
+    APP_ERROR_CHECK(err_code);
+
+    nrf_drv_twi_enable(&m_twi);
+}
+
+/**
+ * @brief Function for main application entry.
+ */
+int main(void)
+{
+    ret_code_t err_code;
+    uint8_t address;
+    uint8_t sample_data;
+    bool detected_device = false;
+
+    SEGGER_RTT_WriteString(0, "TWI scanner.\r\n");
+    twi_init();
+
+    nrf_gpio_cfg_output(RST_PIN);
+    nrf_delay_ms(40);
+    nrf_gpio_pin_set(RST_PIN);
+    nrf_delay_ms(40);
+    nrf_gpio_pin_clear(RST_PIN);
+    nrf_delay_ms(40);
+    nrf_gpio_pin_set(RST_PIN);
+
+    char buff[128];
+
+    for (address = 3; address <= TWI_ADDRESSES; address++)
+    {
+        nrf_delay_ms(40);
+        err_code = nrf_drv_twi_rx(&m_twi, address, &sample_data, sizeof(sample_data));
+
+        if (err_code == NRF_SUCCESS)
+        {
+            detected_device = true;
+            sprintf(buff, "TWI device detected at 0x%x.\r\n", address);
+            SEGGER_RTT_WriteString(0, buff);
+            nrf_delay_ms(100);
+        }
+    }
+
+    if (!detected_device)
+    {
+        SEGGER_RTT_WriteString(0, "No device was found.\r\n");
+    }
+
+    while (true);
+}
+
+/** @} */
